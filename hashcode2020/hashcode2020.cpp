213bf323d12b5fc77db944e5d9ad64aff50bfb54
TCHIDJO MOYO Noel <ntchidjomoyo@murex.com>
2020-02-21 10:06:55 +0100

    test

--- hashcode2020/hashcode2020.cpp
+++ hashcode2020/hashcode2020.cpp
@@ -1,85 +1,191 @@
-// hashcode2020.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <vector>
+#include <set>
+#include <algorithm>
 
-void split(const std::string& str, const std::string& delimiter, std::vector<std::string>& results)
+using namespace std;
+
+int M, number;
+
+int best = 0;
+vector<int> solution;
+vector<int> p;
+vector<int> s;
+
+int B, L, D;
+struct Book
 {
-   std::string::size_type pos = 0, cur = 0;
+   int id;
+   int score;
+   bool done;
+};
 
-   while ((pos = str.find(delimiter.c_str(), cur)) != std::string::npos)
-   {
-      std::string word = str.substr(cur, pos - cur);
-      results.emplace_back(word);
-      cur = pos + 1;
-   }
+vector<Book> books;
 
-   std::string word = str.substr(cur, str.length() - cur);
-   results.emplace_back(word);
-}
-
-void read_input(const std::string& filename)
+struct Lib
 {
-   std::fstream f;
-   f.open(filename.c_str(), std::ios_base::in);
-   if (f.is_open())
+   int id;
+   int N;
+   int T;
+   int M;
+   set<int> booksId;
+   double naiveRatio;
+
+   Lib(int _id, int n, int t, int m, set<int>& bks) :
+      id(_id), N(n), T(t), M(m), booksId(bks)
    {
-      std::string line;
-      while (std::getline(f, line))
+      int sum = 0;
+      for (int id : booksId)
       {
-         std::vector <std::string> tokens;
-         split(line, " ", tokens);
-
-         // insert tokens in the correct data structure test
+         sum += books[id].score;
       }
+      naiveRatio = ((double)sum) / M;
    }
-   else
-   {
-      std::cout << "Cannot open file " << filename << "\n";
-   }
-   f.close();
-}
 
-void write_output(const std::string& filename)
+   void updateratio()
+   {
+      int sum = 0;
+      for (int id : booksId)
+      {
+         if(!books[id].done)
+         sum += books[id].score;
+      }
+      naiveRatio = ((double)sum) / M;
+   }
+};
+
+vector<Lib> libs;
+
+struct Naive
 {
-   std::fstream f;
-   f.open(filename.c_str(), std::ios_base::out);
-
-   if (f.is_open())
+   bool operator()(Lib& a, Lib& b) const
    {
-      //f << some_value;
-      //f << " ";
-      //f << "\n";
+      /*if (a.T != b.T)
+      {
+         return a.T < b.T;
+      }
+      else
+      {
+         return a.naiveRatio > b.naiveRatio;
+      }*/
+      if (a.naiveRatio != b.naiveRatio)
+      {
+         return a.naiveRatio > b.naiveRatio;
+      }
+      else
+      {
+         return a.T < b.T;
+      }
+      /*if (a.M != b.M)
+      {
+         return a.M < b.M;
+      }
+      else
+      if (a.naiveRatio != b.naiveRatio)
+      {
+         return a.naiveRatio > b.naiveRatio;
+      }
+      else
+      {
+         return a.T < b.T;
+      }*/
+      /*if (a.T != b.T)
+      {
+         return a.T < b.T;
+      }
+      else
+         if (a.M != b.M)
+         {
+            return a.M < b.M;
+         }
+         else 
+         {
+            return a.naiveRatio > b.naiveRatio;
+         }*/
+   }
+};
+
+int main(int argc, const char* argv[])
+{
+   string inputFile(argv[1]);
+   ifstream input(inputFile);
+   input >> B >> L >> D;
+
+   for (int i = 0; i < B; ++i)
+   {
+      int s;
+      input >> s;
+      books.push_back({ i,s,false });
+   }
+
+   for (int i = 0; i < L; i++)
+   {
+      int n, t, m;
+      input >> n >> t >> m;
+      set<int> books;
+      for (int j = 0; j < n; ++j)
+      {
+         int d;
+         input >> d;
+         books.insert(d);
+      }
+      libs.push_back({ i, n, t, m, books });
+   }
+
+   Naive comp;
+   std::sort(libs.begin(), libs.end(), comp);
+
+   ofstream file;
+   file.open("output.txt");
+   file << L << endl;
+
+   auto iter = libs.begin();
+   std::vector<Lib> solutions;
+   while (iter != libs.end())
+   {
+      Lib& lib = *iter;
+      solutions.push_back(lib);
+      auto& bks = (*iter).booksId;
+
+      for (int bookId : bks)
+      {
+         books[bookId].done = true;
+      }
+      iter = libs.erase(iter);
+      //update ratio
+      for (auto l : libs)
+      {
+         l.updateratio();
+      }
+      std::sort(libs.begin(), libs.end(), comp);
+   }
+
+   for (int i = 0; i < solutions.size(); i++)
+   {   
+      Lib& lib = solutions[i];
+      file << lib.id << " " << lib.N << endl;
+      auto& bks = lib.booksId;
+      for (int bookId : bks)
+      {
+            file << bookId << " ";         
+      }
+      file << endl;
+
+      //delete books already done
+      //for (int bookId : bks)
+      //{
+      //   for (auto l : libs)
+      //   {
+      //      l.booksId.erase(bookId);
+      //   }
+      //}
 
    }
-   else
-   {
-      std::cout << "Cannot open file " << filename << "\n";
-   }
+
    
-   f.close();
-}
+   file.close();
 
-int main(int argc, char* argv[])
-{
-   if (argc != 3)
-   {
-      std::cout << "hashcode2020.exe [input_file] [output_file]\n";
-      return 1;
-   }
-
-   std::string input = argv[1];
-   std::string output = argv[2];
-
-   read_input(input);
-
-
-
-   write_output(output);
-
-
-    return 0;
+   cout << "End" << endl;
+   return 0;
 }
